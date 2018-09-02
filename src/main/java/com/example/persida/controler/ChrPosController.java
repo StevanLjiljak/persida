f97b612df736108843a81f7290ba9859e6c337cb
scarface-PC\scarface <dsmljiljak@yahoo.com>
2018-09-02 20:47:26 +0200

    Upload file with "," delimiter

--- src/main/java/com/example/persida/controler/ChrPosController.java
+++ src/main/java/com/example/persida/controler/ChrPosController.java
@@ -1,11 +1,7 @@
 package com.example.persida.controler;
 
 import com.example.persida.file.UploadFileResponse;
-import com.example.persida.model.ChrPos;
-import com.example.persida.model.ChrPosId;
 import com.example.persida.service.ChrPosService;
-import com.univocity.parsers.tsv.TsvParser;
-import com.univocity.parsers.tsv.TsvParserSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 @RestController
 @RequestMapping("/api")
