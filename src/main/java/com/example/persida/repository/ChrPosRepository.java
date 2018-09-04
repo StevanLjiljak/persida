f91d75c1779e87ecfa19e6db07bfe012a38159f7
scarface-PC\scarface <dsmljiljak@yahoo.com>
2018-09-04 03:11:34 +0200

    Backend done!

--- src/main/java/com/example/persida/repository/ChrPosRepository.java
+++ src/main/java/com/example/persida/repository/ChrPosRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ChrPosRepository extends JpaRepository<ChrPos,ChrPosId> {
+public interface ChrPosRepository extends JpaRepository<ChrPos, ChrPosId> {
 
     public ChrPos findByChrPosId(ChrPosId chrPosId);
 
@@ -19,7 +19,25 @@ public interface ChrPosRepository extends JpaRepository<ChrPos,ChrPosId> {
     @Query("from chrpos where chrPosId.pos between :pos1 and :pos2")
     public List<ChrPos> getBetweenPos(@Param("pos1") Long pos1, @Param("pos2") Long pos2);
 
-    @Query("from chrpos")
+    /*
+        SELECT *
+        FROM persida.chrpos cp1
+        WHERE cp1.ref IN (
+		    SELECT cp2.ref
+		    FROM persida.chrpos cp2
+		    GROUP BY cp2.ref
+		    HAVING COUNT(cp2.ref) > 1
+        ) GROUP BY cp1.alt
+        HAVING COUNT(cp1.alt) = 1;
+    */
+
+    @Query("from chrpos cp1 " +
+            "where cp1.ref IN ( " +
+            "select cp2.ref from chrpos cp2 " +
+            "group by cp2.ref " +
+            "having count(cp2.ref) > 1 " +
+            ") group by cp1.alt " +
+            " having count(cp1.alt) = 1")
     public List<ChrPos> getDeletion();
 
 }
